3d80e2591c8faf7503af4893777b7f5cb07cb3d9
Teifion <teifion@teifion.co.uk>
2025-01-22 00:33:04 +0000

    Bugfixes

--- priv/repo/migrations/20250122000318_create_game_universes_table.exs
+++ priv/repo/migrations/20250122000318_create_game_universes_table.exs
@@ -1,12 +1,14 @@
 defmodule Durandal.Repo.Migrations.CreateGameUniversesTable do
   use Ecto.Migration
 
-  create_if_not_exists table(:game_universes, primary_key: false) do
-    add(:id, :uuid, primary_key: true, null: false)
+  def change do
+    create_if_not_exists table(:game_universes, primary_key: false) do
+      add(:id, :uuid, primary_key: true, null: false)
 
-    add(:name, :string)
-    add(:active?, :boolean)
+      add(:name, :string)
+      add(:active?, :boolean)
 
-    timestamps(type: :utc_datetime)
+      timestamps(type: :utc_datetime)
+    end
   end
 end
