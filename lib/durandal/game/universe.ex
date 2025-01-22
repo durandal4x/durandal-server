8905a869dc4606ff31ef442ad4a4ea12aab8c72c
Teifion <teifion@teifion.co.uk>
2025-01-22 00:03:57 +0000

    Added universes table

---
+++ lib/durandal/game/universe.ex
@@ -0,0 +1,40 @@
+defmodule Durandal.Game.Universe do
+  @moduledoc """
+  # Universe
+  Description here
+
+  ### Attributes
+  * `:name` - field description
+  * `:active?` - field description
+  """
+  use Ecto.Schema
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  schema "game_universes" do
+    field(name, :string)
+    field(active?, :boolean)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @type id :: Ecto.UUID.t()
+
+  # @type t :: %__MODULE__{
+  #         id: id(),
+  #         name: String.t(),,
+  #         active?: boolean(),
+  #         inserted_at: DateTime.t(),
+  #         updated_at: DateTime.t()
+  #       }
+
+  @required_fields ~w(name active?)a
+  @optional_fields ~w()a
+
+  @doc false
+  @spec changeset(map(), map()) :: Ecto.Changeset.t()
+  def changeset(struct, attrs \\ %{}) do
+    struct
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+  end
+end
---
+++ test/durandal/universe_lib_test.exs
@@ -0,0 +1,122 @@
+defmodule Durandal.UniverseLibTest do
+  @moduledoc false
+  alias Durandal.Game.Universe
+  alias Durandal.Game
+  use Durandal.Case, async: true
+
+  alias Durandal.{GameFixtures, AccountFixtures}
+
+  defp valid_attrs do
+    %{
+      name: "Some name",
+      active?: true
+    }
+  end
+
+  defp update_attrs do
+    %{
+      name: "Some updated name",
+      active?: false
+    }
+  end
+
+  defp invalid_attrs do
+    %{
+      name: nil,
+      active?: nil
+    }
+  end
+
+  describe "universe" do
+    alias Durandal.Game.Universe
+
+    test "universe_query/0 returns a query" do
+      q = Game.universe_query([])
+      assert %Ecto.Query{} = q
+    end
+
+    test "list_universe/0 returns universe" do
+      # No universe yet
+      assert Game.list_universes([]) == []
+
+      # Add a universe
+      GameFixtures.universe_fixture()
+      assert Game.list_universes([]) != []
+    end
+
+    test "get_universe!/1 and get_universe/1 returns the universe with given id" do
+      universe = GameFixtures.universe_fixture()
+      assert Game.get_universe!(universe.id) == universe
+      assert Game.get_universe(universe.id) == universe
+    end
+
+    test "create_universe/1 with valid data creates a universe" do
+      assert {:ok, %Universe{} = universe} =
+               Game.create_universe(valid_attrs())
+
+      assert universe.name == "some name"
+    end
+
+    test "create_universe/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Game.create_universe(invalid_attrs())
+    end
+
+    test "create_universe/4 with valid data creates a universe" do
+      user_id = AccountFixtures.user_fixture().id
+
+      assert {:ok, %Universe{} = universe} =
+               Game.create_universe(user_id, "ip", "some name", %{})
+
+      assert universe.name == "some name"
+    end
+
+    test "create_universe/4 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Game.create_universe(nil, nil, nil, nil)
+    end
+
+    test "create_anonymous_universe/3 with valid data creates a universe" do
+      assert {:ok, %Universe{} = universe} =
+               Game.create_anonymous_universe("ip", "some name", %{})
+
+      assert universe.name == "some name"
+    end
+
+    test "create_anonymous_universe/3 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Game.create_anonymous_universe(nil, nil, nil)
+    end
+
+    test "update_universe/2 with valid data updates the universe" do
+      universe = GameFixtures.universe_fixture()
+
+      assert {:ok, %Universe{} = universe} =
+               Game.update_universe(universe, update_attrs())
+
+      assert universe.name == "some updated name"
+    end
+
+    test "update_universe/2 with invalid data returns error changeset" do
+      universe = GameFixtures.universe_fixture()
+
+      assert {:error, %Ecto.Changeset{}} =
+               Game.update_universe(universe, invalid_attrs())
+
+      assert universe == Game.get_universe!(universe.id)
+    end
+
+    test "delete_universe/1 deletes the universe" do
+      universe = GameFixtures.universe_fixture()
+      assert {:ok, %Universe{}} = Game.delete_universe(universe)
+
+      assert_raise Ecto.NoResultsError, fn ->
+        Game.get_universe!(universe.id)
+      end
+
+      assert Game.get_universe(universe.id) == nil
+    end
+
+    test "change_universe/1 returns a universe changeset" do
+      universe = GameFixtures.universe_fixture()
+      assert %Ecto.Changeset{} = Game.change_universe(universe)
+    end
+  end
+end
